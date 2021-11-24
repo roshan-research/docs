4f1efd7ff3be9371c4037e4cc977e4d0bc95645a
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-24 06:02:34 +0330

    added the needed imports to the java file

--- apiCodes/alefba/MyRequestLocal.java
+++ apiCodes/alefba/MyRequestLocal.java
@@ -6,6 +6,8 @@ import java.nio.charset.StandardCharsets;
 import java.net.URLConnection;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 
 class MyRequest {
 
