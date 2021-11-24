deef9cc1bb4f8fcdea643602a42e273b1d03c0c0
Muhammad Karbalaee <muhammad.ksht@gmail.com>
2021-11-24 07:41:55 +0330

    initialized the java file to customize

---
+++ apiCodes/MyRequest.java
@@ -0,0 +1,47 @@
+import java.lang.System;
+import java.net.HttpURLConnection;
+import java.io.OutputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.net.URLConnection;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+class MyRequest {
+
+    public static void main(String[] args){
+        try{
+            URL url = new URL("https://alefba.roshan-ai.ir/api/read_document/");
+            URLConnection con = url.openConnection();
+            HttpURLConnection http = (HttpURLConnection)con;
+            http.setRequestMethod("POST");
+            http.setDoOutput(true);
+
+            byte[] out = "{
+    "document_url": "http://bayanbox.ir/view/5067853395275628881/boute.pdf",
+    "fix_orientation": true,
+    "word_positions": false,
+    "type": "general",
+    "wait": true
+}".getBytes(StandardCharsets.UTF_8);
+            int length = out.length;
+
+            http.setFixedLengthStreamingMode(length);
+            http.setRequestProperty("Content-Type", "application/json");
+            http.setRequestProperty("Authorization", "Token TOKEN_KEY");
+            http.connect();
+            try(OutputStream os = http.getOutputStream()) {
+                os.write(out);
+            }
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(http.getInputStream()));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null)
+                System.out.println(inputLine);
+            in.close();
+        }
+        catch(Exception e){
+            System.err.println(e);
+        }
+    }
+}
