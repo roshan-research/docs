5a64cb2ffd8833ea7c3d71b8f5eb973902a1f31c
Muhammad Karbalaee Shabani <77221151+muhammadksht@users.noreply.github.com>
2021-11-30 19:59:50 +0330

    multipart request final version in java

--- apiCodes/alefba/MultiPartRequest.java
+++ apiCodes/alefba/MultiPartRequest.java
@@ -6,19 +6,20 @@ public class MultiPartRequest {
   public static void main(String[] args) throws IOException {
 
     String url = "https://alefba.roshan-ai.ir/api/read_document/";
-    String charset = "UTF-8";
     File textFile = new File("YOUR FILE PATH");
     String boundary = Long.toHexString(System.currentTimeMillis());
     String CRLF = "\r\n";
 
     URLConnection connection = new URL(url).openConnection();
     connection.setDoOutput(true);
-    connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
-    connection.setRequestProperty("Authorization", "Token TOKEN_KEY");
     connection.setRequestProperty("accept", "*/*");
+    connection.setRequestProperty("Connection", "close");
+    connection.setRequestProperty("Authorization", "Token TOKEN_KEY");
+    connection.setRequestProperty("Content-Type", "multipart/form-data; boundary=" + boundary);
+
     try (
         OutputStream output = connection.getOutputStream();
-        PrintWriter writer = new PrintWriter(new OutputStreamWriter(output, charset), true);
+        PrintWriter writer = new PrintWriter(new OutputStreamWriter(output), true);
     ) {
       writer.append("--").append(boundary).append(CRLF);
       writer.append("Content-Disposition: form-data; name=\"document\"; filename=\"").append(textFile.getName()).append("\"").append(CRLF);
@@ -29,6 +30,7 @@ public class MultiPartRequest {
       writer.append(CRLF).flush();
       writer.append("--").append(boundary).append("--").append(CRLF).flush();
     }
+
 
     BufferedReader inputStream = new BufferedReader(new InputStreamReader((InputStream) connection.getContent()));
     String inputLine;
