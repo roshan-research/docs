14749ebd889edde1a00ddb3c25b0b9d5d22e46cb
Ayub <mrkou65@gmail.com>
2022-12-01 12:52:15 +0330

    Improve script to install dependencies automatically, if required

--- update.sh
+++ update.sh
@@ -1,6 +1,29 @@
-set -e
+# In the windows, follow these steps to run the script:
+# 1. Install wsl (https://learn.microsoft.com/en-us/windows/wsl/install)
+# 2. Reopen this project with the wsl in the vscode
+# 3. Run the script again.
 
-echo 'RUN THE SCRIPT AS SUPER USER'
+echo "Installing dependencies if needed..."
+
+
+# Ubuntu
+if [ "$(. /etc/os-release; echo $NAME)" = "Ubuntu" ]; then
+  sudo apt install ruby ruby-dev build-essential libffi-dev zlib1g-dev liblzma-dev nodejs patch
+  sudo apt-get update -y
+  sudo apt-get install -y bundler
+fi
+
+# Mac
+if [ "$(uname)" == "Darwin" ]; then
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+    xcode-select --install
+    sudo xcodebuild -license
+    brew install node
+    gem update --system
+    gem install bundler
+fi
+
+echo "Start building project..."
 
 cd slate_project
 bundle install --quiet
@@ -21,8 +44,8 @@ echo 'copied all markdown files to the slate_project folder'
 build_one(){    
     rm ./source/index.html.md
     mv ./source/$1.json.md ./source/index.html.md
-    cp ../logos/$1/logo.svg ./source/images/
-    bundle exec middleman build
+    cp ../logos/$1/logo.svg ./source/images/    
+    bundle exec middleman build    
     rm -r ../docs/$1/*
     cp -r ./build/* ../docs/$1
     echo "$1 documentations are updated"
