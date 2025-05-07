054f81ffb1fc6a624d61641384ba7d83dc9701b2
Ayub Kokabi <sir.kokabi@gmail.com>
2025-05-07 16:56:25 +0330

    Update JsAppender.js

--- JsAppender.js
+++ JsAppender.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-let folderNames = ['alefba','baaz','golrokh','harf','kashf','naghsh','parde',"replai","fahm"];
+let folderNames = ['alefba','baaz','golrokh','harf','kashf','naghsh','parde',"replai","fahm","barid"];
 
 let scrollingCode = fs.readFileSync("scrolling.js",'utf8',() => {});
 
